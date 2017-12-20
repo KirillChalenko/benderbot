8e9ae73b5288ee724d2f1a4c47ec94e026b5b635
Kirill Chalenko <kiryl.chily@gmail.com>
2017-12-20 19:23:06 +0300

    removed unused targets

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "BenderBot",
     targets: [
-        Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"]),
+        Target(name: "Run")
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
